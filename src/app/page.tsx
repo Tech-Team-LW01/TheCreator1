c21738e230db7ffd0c26b3cd9e72cac2d03db9a6
Bhupesh Choudhary <bhaibhupesh10@gmail.com>
2025-01-29 18:40:45 +0530

    udpated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import TechStack from "@/components/customComponents/Technology/Technology";
 import {TopLeaders} from "@/components/customComponents/TopLeaders/TopLeaders";
 import {LeftRightAlumniCarousel} from "@/components/customComponents/CompaniesCarousel/CompaniesCarousel";
 import Projets from "@/components/customComponents/Projects/Project";
-import {OutCome}  from "@/components/customComponents/OutCome/OutCome";
+// import {OutCome}  from "@/components/customComponents/OutCome/OutCome";
 import VideoTestimonials from "@/components/customComponents/Testimonials/VideoTestimonials";
 import FactAndFiqure from "@/components/customComponents/FactAndFigures/FactAndFiqure";
 import Awards from "@/components/customComponents/Awards/Awards";
