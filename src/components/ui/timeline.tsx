4fba6a53aced0f82a266c827c702d2700825fe12
Sumit <tonystark83033@gmail.com>
2025-02-05 16:53:38 +0530

    make time responsive

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -34,7 +34,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div
-      className="w-full bg-black text-white dark:bg-neutral-950 font-sans md:px-10"
+      className="w-full bg-black text-white dark:bg-neutral-950 font-sans px-2 sm:px-4 md:px-10"
       ref={containerRef}
     >
       {/* <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
@@ -46,13 +46,11 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
           a timeline of my journey.
         </p>
       </div> */}
-
-      <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
+      
+  <div ref={ref} className="relative max-w-7xl mx-auto pb-10 sm:pb-16 md:pb-20">
+     
         {data.map((item, index) => (
-          <div
-            key={index}
-            className="flex justify-start pt-10 md:pt-40 md:gap-10"
-          >
+          <div key={index} className="flex justify-start pt-6 sm:pt-20 md:pt-40 md:gap-10">
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-48">
               <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-black dark:bg-black flex items-center justify-center">
                 <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
