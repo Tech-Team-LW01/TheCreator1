7de1b944ca9ae780f1d6ed0a3e096d52df46b345
anshuraghv12 <134767699+anshuraghv12@users.noreply.github.com>
2025-02-06 18:46:35 +0530

    update jazbaa

--- src/components/customComponents/JazbaaTimeLine/StudentProfileCard.tsx
+++ src/components/customComponents/JazbaaTimeLine/StudentProfileCard.tsx
@@ -12,7 +12,7 @@ const StudentProfileCard = () => {
       <div className="flex-shrink-0">
         <div className="w-12 h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 rounded-full overflow-hidden bg-gray-100">
           <img
-            src="https://media.licdn.com/dms/image/v2/D4D03AQHutY1ZyT8dwA/profile-displayphoto-shrink_800_800/B4DZSxVzAiH0Ac-/0/1738142071879?e=1744243200&v=beta&t=gSO-cOcSAbS0yUO88P2u_JLP4DkV05l0kneFHozc_2s"
+            src="https://www.linkedin.com/in/nareshkumarjyani/"
             alt="Student"
             className="w-full h-full object-cover"
           />
@@ -24,18 +24,18 @@ const StudentProfileCard = () => {
         {/* Top row with name */}
         <div className="flex justify-between items-start">
           <h3 className="text-xs sm:text-sm font-semibold text-[#ff0000] truncate max-w-[150px] sm:max-w-[200px]">
-            Sumit pal 
+          Naresh Kumar
           </h3>
         </div>
 
         {/* Company section */}
         <div className="flex flex-col">
           <p className="text-[8px] sm:text-[9px] p-[2px] text-black font-semibold line-clamp-2">
-            Senior Frontend Developer
+             Associate - Software Engineer
           </p>
           <div className="h-4 sm:h-5 w-12 sm:w-16 mb-1">
             <img
-              src="/assets/google.png"
+              src="/assets/timeline/2014/logo_unthinkable_dbd9877981.png"
               alt="Company Logo"
               className="h-full w-full object-contain"
             />
