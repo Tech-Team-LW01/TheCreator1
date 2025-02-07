f17a99efbfb2791436d93ebe028b3249a7b0b16a
Sumit <tonystark83033@gmail.com>
2025-02-07 17:13:14 +0530

    fix mogo db url

--- src/app/api/submit-application/route.ts
+++ src/app/api/submit-application/route.ts
@@ -27,8 +27,8 @@ export async function POST(req: Request) {
       port: 587,
       secure: false,
       auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
+        user: process.env.SMTP_USER || '  ',
+        pass: process.env.SMTP_PASSWORD || '  ',
       },
     });
 
