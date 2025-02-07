7701f7815e25a92915c8f2fd6c8560361d4b0b9b
Sumit <tonystark83033@gmail.com>
2025-02-07 19:03:25 +0530

    update award

--- src/components/customComponents/Awards/Awards.tsx
+++ src/components/customComponents/Awards/Awards.tsx
@@ -23,11 +23,11 @@ const Award = () => {
 </div>
 
             {/* Center Section */}
-            <div className="flex flex-col text-center sm:text-left mx-40 z-10 mt-4 sm:mt-0"> {/* Added mx-auto, removed sm:ml-8 sm:mr-auto */}
-              <h2 className="text-[#000000] text-[20px] sm:text-2xl md:text-3xl px-2 sm:px-4 font-bold  leading-tight text-center">
-                <span className='text-[#ff0000]'>Awarded as #1 </span>
+            <div className="flex flex-col text-center sm:text-left mx-20 z-10 mt-2 sm:mt-0"> {/* Added mx-auto, removed sm:ml-8 sm:mr-auto */}
+              <h2 className="text-[#000000] text-[20px] sm:text-2xl md:text-4xl px-2 sm:px-4 font-bold  leading-tight text-center">
+                <span className='text-[#ff0000] '>Awarded as #1 </span>
                 <br className="hidden sm:block"/>
-                <span className="sm:hidden"> </span>
+                <span className=""> </span>
                 for Internship in India & Asia
               </h2>
             </div>
@@ -36,7 +36,7 @@ const Award = () => {
             <div className="absolute left-1/2 -translate-x-1/2 sm:left-auto sm:translate-x-0 sm:right-10 -top-4">
               <div className="relative   border-white">
                 <img
-                  className='relative w-[120px] h-[120px] sm:w-[150px] sm:h-[150px]  object-cover'
+                  className='relative w-[270px] h-320px]   object-cover'
                   src='/assets/Award/award-image.png'
                   alt="Award"
                 />
