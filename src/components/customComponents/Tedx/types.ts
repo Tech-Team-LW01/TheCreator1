2a0c2dfb3dce1bc74bc77111d17e16c82fd71b03
Sumit <tonystark83033@gmail.com>
2025-02-11 17:21:58 +0530

    fix tadx

--- src/components/customComponents/Tedx/types.ts
+++ src/components/customComponents/Tedx/types.ts
@@ -1,13 +1,12 @@
-// types.ts
 export interface SpeakerProps {
     imageUrl: string;
-  
     badge: string;
     firstName: string;
     lastName: string;
     link: string;
- 
-    description?: string;
-    
-    onClick?: () => void;
-}
+    socialLinks?: {
+      instagram?: string;
+      facebook?: string;
+      linkedin?: string;
+    };
+  }
