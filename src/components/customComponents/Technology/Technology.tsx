a085b99346659c7450b54149b63062c5a8fc3c6e
anshuraghv12 <134767699+anshuraghv12@users.noreply.github.com>
2025-02-13 12:11:41 +0530

    update code

--- src/components/customComponents/Technology/Technology.tsx
+++ src/components/customComponents/Technology/Technology.tsx
@@ -38,7 +38,7 @@ export default function TechStack() {
     <section className="container bg-black mx-auto px-4 py-8">
       <div className="grid mx-auto gap-8  lg:grid-cols-2 lg:gap-12 items-center max-w-6xl">
         <div className="space-y-4 w-full mx-auto">
-          <h1 className="text-4xl text-[#ff0000] font-bold tracking-tight text-center sm:text-left lg:text-4xl">
+          <h1 className="text-3xl text-[#ff0000] font-bold tracking-tight text-center sm:text-left lg:text-4xl">
             Learn, Research, Integrate & Build Industry Live Summer Project
           </h1>
           <p className={`text-sm md:text-3xl  text-white hidden sm:block ${poppins.className}`}>
