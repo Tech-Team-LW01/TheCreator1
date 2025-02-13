6c4eee2dea3c3922da5839484ae5e6782bb29111
anshuraghv12 <134767699+anshuraghv12@users.noreply.github.com>
2025-02-13 12:26:14 +0530

    updaet code

--- src/components/customComponents/Mentor/Mentor.tsx
+++ src/components/customComponents/Mentor/Mentor.tsx
@@ -37,7 +37,7 @@ const Mentor = () => {
           {/* Left Section */}
           <div className="w-full lg:w-1/2 space-y-6">
             <div className="prose prose-invert max-w-none">
-              <h1 className="text-2xl md:text-4xl lg:text-4xl text-center font-bold mb-3">
+              <h1 className="text-3xl md:text-4xl lg:text-4xl text-center font-bold mb-3">
                 <p className="text-[#ff0000]">Your Summer Internship Mentor : <br/> Mr Vimal Daga</p>
               </h1>
               
