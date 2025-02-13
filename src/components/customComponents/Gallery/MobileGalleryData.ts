64eeeb150b84c7d01651cdcf32e19da806d92973
anshuraghv12 <134767699+anshuraghv12@users.noreply.github.com>
2025-02-13 16:44:23 +0530

    updaet code

--- src/components/customComponents/Gallery/MobileGalleryData.ts
+++ src/components/customComponents/Gallery/MobileGalleryData.ts
@@ -83,4 +83,50 @@ export const bentoItems1 = [
       interval: 10000,
       transitionDuration: 1,
     },
+   
+    
+  ];
+  export const bentoItems3 = [
+    {
+      id: 1,
+      title: "Settings Menu",
+      images: [
+        "/assets/event-gallery/G3.jpeg",
+        "/assets/event-gallery/G13 copy.jpg",
+        "/assets/event-gallery/G6.jpeg",
+      ],
+      width: 4,
+      height: 2,
+      interval: 5000,
+      transitionDuration: 1,
+    },
+    {
+      id: 2,
+      title: "Email Preview",
+      images: [
+        "/assets/event-gallery/G15.jpeg",
+        "/assets/event-gallery/G18.jpg",
+        "/assets/event-gallery/G7.jpg",
+      ],
+      width: 4,
+      height: 2,
+      interval: 20000,
+      transitionDuration: 1.5,
+    },
+
+    {
+      id: 3,
+      title: "Settings Menu",
+      images: [
+        "/assets/event-gallery/G12.jpg",
+        "/assets/event-gallery/G21.jpg",
+        "/assets/event-gallery/G20.jpg",
+      ],
+      width: 8,
+      height: 2,
+      interval: 10000,
+      transitionDuration: 1,
+    },
+   
+    
   ];
